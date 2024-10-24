f79dbe9e0cc3e9c34c0015e76746cd03e71ecad9
Michael <michaelshalom57@gmail.com>
2024-10-24 05:38:04 -0700

    restful crud

--- server/app.py
+++ server/app.py
@@ -46,7 +46,33 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response(jsonify({"message": "Plant not found"}), 404)
 
+        # Handle JSON input to update plant attributes
+        data = request.get_json()
+        
+        for attr, value in data.items():
+            setattr(plant, attr, value)
+        
+        db.session.add(plant)
+        db.session.commit()
+        
+        response_dict = plant.to_dict()
+        return make_response(jsonify(response_dict), 200)
+        
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response(jsonify({"message": "Plant not found"}), 404)
+        
+        db.session.delete(plant)
+        db.session.commit()
+        
+        return '', 204  
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
